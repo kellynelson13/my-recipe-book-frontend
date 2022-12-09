3caff83624a0ce1cc4d276be1d4e5ddcce4815df
Kelly Nelson <kelnel86@gmail.com>
2022-12-09 11:06:06 -0600

    adds fetch call for appetizers

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Switch, Route} from 'react-router-dom';
 import Home from '../pages/Home';
 import Appetizers from '../pages/Appetizers';
@@ -9,6 +9,18 @@ const Main = () => {
   const [appetizers, setAppetizers] = useState([])
   const [entrees, setEntrees] = useState([])
   const [desserts, setDesserts] = useState([])
+
+  const appetizersURL = "https://my-recipe-book-13.herokuapp.com/appetizers";
+
+  const getAppetizers = async () => {
+    const response = await fetch(appetizersURL);
+    const data = await response.json();
+    setAppetizers(data);
+  }
+
+  useEffect(() => {
+    getAppetizers()
+  }, [])
 
   return (
     <main>
