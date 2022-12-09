2b0d8c4acc5db379f2a41213e029c00bce0ff6b6
Kelly Nelson <kelnel86@gmail.com>
2022-12-09 10:30:33 -0600

    adds 3 states to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Switch, Route} from 'react-router-dom';
 import Home from '../pages/Home';
 import Appetizers from '../pages/Appetizers';
 import AppetizersShow from '../pages/AppetizersShow';
 
 const Main = () => {
+
+  const [appetizers, setAppetizers] = useState([])
+  const [entrees, setEntrees] = useState([])
+  const [desserts, setDesserts] = useState([])
+
   return (
     <main>
       <Switch>
