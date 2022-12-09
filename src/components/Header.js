3ff1df72c603b9fcd86eee6bab62ac57716160e5
Kelly Nelson <kelnel86@gmail.com>
2022-12-09 10:53:37 -0600

    adds nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+      <Link to="/">
+        <div>
+          My Recipe Book
+        </div>
+      </Link>
+    </nav>
   )
 }
 
