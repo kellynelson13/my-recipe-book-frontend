fb0af2af491602d2eb34d1857e5fd10e4e40c72f
Kelly Nelson <kelnel86@gmail.com>
2023-01-09 11:35:17 -0600

    accessing specific appetizer in show page

--- src/pages/AppetizersShow.js
+++ src/pages/AppetizersShow.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const AppetizersShow = () => {
+const AppetizersShow = (props) => {
+
+    const id = props.match.params.id
+    const appetizers = props.appetizers
+    const appetizer = appetizers.find(ap => ap._id === id)
+
   return (
     <div>AppetizersShow</div>
   )
