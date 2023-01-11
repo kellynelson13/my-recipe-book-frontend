b9f5bd6e50c7e9f271c04ce8a8e11029685dde94
Kelly Nelson <kelnel86@gmail.com>
2023-01-11 11:25:40 -0600

    adds update form

--- src/pages/AppetizersShow.js
+++ src/pages/AppetizersShow.js
@@ -20,16 +20,67 @@ const AppetizersShow = (props) => {
 
   return (
     <div>
-      <h1>{appetizer.name}</h1>
-      <img src={appetizer.img} alt={appetizer.name}/>
-      <ul>
-      {appetizer.ingredients.map(i => {
-        return(
-          <li>{i.ingredient}</li>
-        )
-      })}
-      </ul>
-      <p>{appetizer.instructions}</p>
+      <div>
+        <h1>{appetizer.name}</h1>
+        <img src={appetizer.img} alt={appetizer.name}/>
+        <ul>
+        {appetizer.ingredients.map(i => {
+          return(
+            <li>{i.ingredient}</li>
+          )
+        })}
+        </ul>
+        <p>{appetizer.instructions}</p>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label> <span>Name of Recipe</span>
+          <input 
+            type="text"
+            value={editForm.name}
+            name="name"
+            placeholder="Name of Dish"
+            onChange={handleChange}
+          />
+        </label>
+        <label> <span>Image of Recipe</span>
+          <input 
+            type="text"
+            value={editForm.img}
+            name="img"
+            placeholder="Image URL"
+            onChange={handleChange}
+          />
+        </label>
+        <label> <span>Ingredients</span>
+          <input 
+            type="text"
+            value={editForm.ingredients}
+            name="ingredients"
+            placeholder="Ingredient"
+            onChange={handleChange}
+          />
+          {/* <button onClick={handleIngredientsClick} >Add Ingredient</button> */}
+        </label>
+            {/* {ingredients.map((ing) => {
+            return (
+              <div>
+                {ing.ingredient}
+              </div>
+            )
+          })} */}
+        <label> <span>Instructions</span>
+          <textarea
+            name="instructions"
+            placeholder=" Add instructions"
+            onChange={handleChange}
+            value={editForm.instructions}
+          > 
+            {editForm.instructions}
+          </textarea>
+        </label>
+        <input type="submit" value="Add Recipe"/>
+      </form>
     </div>
   )
 }
