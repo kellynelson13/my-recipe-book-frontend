f613710e16eef5caf159ceda229742b8efcdf257
Kelly Nelson <kelnel86@gmail.com>
2023-01-18 11:09:58 -0600

    accesses the specific dessert via the id

--- src/pages/DessertsShow.js
+++ src/pages/DessertsShow.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const DessertsShow = () => {
+const DessertsShow = (props) => {
+
+    const id = props.match.params.id
+    const desserts = props.desserts
+    const dessert = desserts.find(de => de._id === id)
+
   return (
     <div>DessertsShow</div>
   )
